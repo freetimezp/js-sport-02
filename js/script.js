06414f2d0a6db2624bb806c2df40c32a5014c0fc
freetimezp <kraftukrnet@ukr.net>
2023-03-02 22:27:56 +0200

    07. js-sport-02. Services section as accordion.

--- js/script.js
+++ js/script.js
@@ -48,7 +48,40 @@ function video() {
 
 video();
 
+/* accordion */
 
+function accordion(ele) {
+    const accordion = document.querySelector(ele);
+
+    accordion.addEventListener("click", ({target}) => {
+        if(!target.closest(".js-accordion-btn")) {
+            return;
+        }
+
+        const btn = target.closest(".js-accordion-btn");
+        const item = btn.parentElement.parentElement;
+        const body = btn.parentElement.nextElementSibling;
+
+        if(target.closest(".active")) {
+            slideUp();
+            return;
+        }
+
+        if(accordion.querySelector(".active")) {
+            slideUp();
+        }
+
+        item.classList.add("active");
+        body.style.height = body.scrollHeight + "px";
+
+        function slideUp() {
+            accordion.querySelector(".active").lastElementChild.style.height = "0";
+            accordion.querySelector(".active").classList.remove("active");
+        }
+    });
+};
+
+accordion(".js-accordion");
 
 
 
